16e1fa357852c0f7ddd53ea41aada9643469ddcf
aguanteboca <perez.cesaar@gmail.com>
2016-06-06 22:10:53 -0300

    Modificado

--- ConcesionariaTp/src/adjudicacion/Adjudicacion2.java
+++ ConcesionariaTp/src/adjudicacion/Adjudicacion2.java
@@ -6,5 +6,7 @@ import planDeAhorro.PlanDeAhorro;
 public interface Adjudicacion2 {
 
 	Cliente seleccionDeCliente(PlanDeAhorro plan);
+	
+	
 
 }
--- ConcesionariaTp/src/cliente/Cliente.java
+++ ConcesionariaTp/src/cliente/Cliente.java
@@ -29,4 +29,6 @@ public class Cliente {
 		this.planes = new ArrayList<PlanDeAhorro>();
 	}
 
+	
+	pspsp
 }
