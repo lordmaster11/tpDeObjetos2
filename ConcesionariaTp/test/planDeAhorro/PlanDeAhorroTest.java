6ccc2b2910cafd3a4668a0efb1e08d6051c10a64
Marcelo <jmdicostanzo11@gmail.com>
2016-06-17 19:22:00 -0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/lordmaster11/tpDeObjetos2.git
    
    Conflicts:
    	ConcesionariaTp/test/planDeAhorro/PlanDeAhorroTest.java

---
+++ ConcesionariaTp/test/planDeAhorro/PlanDeAhorroTest.java
@@ -0,0 +1,71 @@
+package planDeAhorro;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import adjudicacion.Adjudicacion;
+import cliente.Cliente;
+import comprobanteDePago.ComprobanteDePago;
+import concesionaria.Concesionaria;
+import financiamiento.Financiamiento;
+import modelo.Modelo;
+
+
+public class PlanDeAhorroTest {
+	 
+    PlanDeAhorro planDeAhorro;
+    Cliente clienteMock;
+	Modelo modeloMock;
+	Financiamiento financiamiento70_30Mock;
+	Adjudicacion adjudicacionMock;
+	Concesionaria concesionariaMock;
+       
+    @Before
+    public void setUp() throws Exception {
+    	 
+    	clienteMock = mock(Cliente.class);
+    	modeloMock = mock(Modelo.class);
+    	financiamiento70_30Mock =  mock(Financiamiento.class);
+    	adjudicacionMock = mock(Adjudicacion.class);
+    	planDeAhorro =  new PlanDeAhorro(125, modeloMock, financiamiento70_30Mock, adjudicacionMock, 0, concesionariaMock);
+    
+
+    }
+
+	@Test
+	public void testSuscribirCliente() {
+		
+		planDeAhorro.agregarSuscripto(clienteMock);
+		assertTrue(planDeAhorro.cantSuscriptos().equals(1));
+		
+	}
+	
+	@Test
+	public void testGetSuscriptos() {
+		planDeAhorro.agregarSuscripto(clienteMock);
+		
+		assertTrue (planDeAhorro.cantSuscriptos().equals(1));
+	} 
+
+	@Test
+	public void testValorADesembolsar() {
+		
+		when(modeloMock.getValorVenta()).thenReturn(100000f);
+		when(financiamiento70_30Mock.valorTotalEnCuotas(modeloMock)).thenReturn(70000f);
+		
+		assertTrue((planDeAhorro.valorADesembolzar()).equals(70000f));
+				
+	}			 
+	
+	
+	
+	
+	
+	
+	
+	
+	/// te aparece esto???
+}
